5f914eed9c73d1e33fb04675a29f5beb980bfbdb
qz515880 <1195933889@qq.com>
2017-03-05 11:41:42 +0800

    添加一个模块

--- halo.js
+++ halo.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 var app = express();
 app.use(express.static(__dirname+'/public'));
 
@@ -8,11 +9,6 @@ app.set('view engine','handlebars');
 
 app.set('port',process.env.PORT||3000);
 
-var fortunes = ["Whenever possible, keep it simple.",
-				"Revers need springs.",
-				"Do not fear what you don't know.",
-				"You will have a pleasant suprise.",
-				"Conquer your fears or they will conquer you."];
 
 app.get('/',function(req,res){
 	// res.type('text/plain');
@@ -23,8 +19,7 @@ app.get('/',function(req,res){
 app.get('/about',function(req,res){
 	// res.type('text/plain');
 	// res.send('This is halo about');
-	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-	res.render('about',{fortune:randomFortune});
+	res.render('about',{fortune:fortune.getFortune()});
 });
 
 //404页面
